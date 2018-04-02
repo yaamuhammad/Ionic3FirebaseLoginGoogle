d14358c8190cbd347691ed23b11b43cc5a45f5c1
Imam Asari <tahzan.la10@gmail.com>
2018-04-02 23:42:48 +0700

    Blog.Babanggi.com

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  displayName: any;
+  email: any;
+  familyName: any;
+  givenName: any;
+  userId: any;
+  imageUrl: any;
 
-  constructor(public navCtrl: NavController) {
+  isLoggedIn:boolean = false;
+
+  constructor(public navCtrl: NavController, private googlePlus: GooglePlus) {
 
   }
 
+  login() {
+    this.googlePlus.login({})
+      .then(res => {
+        console.log(res);
+        this.displayName = res.displayName;
+        this.email = res.email;
+        this.familyName = res.familyName;
+        this.givenName = res.givenName;
+        this.userId = res.userId;
+        this.imageUrl = res.imageUrl;
+
+        this.isLoggedIn = true;
+      })
+      .catch(err => console.error(err));
+  }
+
+  logout() {
+    this.googlePlus.logout()
+      .then(res => {
+        console.log(res);
+        this.displayName = "";
+        this.email = "";
+        this.familyName = "";
+        this.givenName = "";
+        this.userId = "";
+        this.imageUrl = "";
+
+        this.isLoggedIn = false;
+      })
+      .catch(err => console.error(err));
+  }
 }
